81847b6bc6f1a4ef7b8ce57c5bcba49faa242323
Mathieu MOUTTAPA <mathm@hotmail.fr>
2017-10-06 18:29:23 +0200

    CompassActivity + MainActivity Created
    Ajout d'une activité simple MainActivity
    Ajout d'une activité simple CompassActivity
    Ajout de bouton dans MainActivity pour passer d'une activité à l'autre

---
+++ app/src/main/java/com/example/mathm/geolocalisationindoor/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mathm.geolocalisationindoor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onMapButtonClicked(View v){
+
+        Intent intent = new Intent(MainActivity.this, CarteActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onCompassButtonClicked(View v){
+
+        Intent intent = new Intent(MainActivity.this, CompassActivity.class);
+        startActivity(intent);
+
+    }
+
+
+}
